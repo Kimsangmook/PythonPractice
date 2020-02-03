762ac39fc4edc91688238b3e343926b1bc0c02d2
Kim Sang Mook <uj03109@naver.com>
2020-02-03 19:49:04 +0900

    first commit

---
+++ 2020.02.01.py
@@ -0,0 +1,11 @@
+#########################
+# Pythonpratice
+
+a=input('write character')
+b=input('write character')
+print(a+b)
+
+x=int(input('write number'))
+y=int(input('write number'))
+print(x+y)
+
